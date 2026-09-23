70188df4fbbe21fb6427aae6b1d2a83cf2f9fc36
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2026-09-24 00:34:31 +0500

    config: add wildcard image remotePatterns to allow all wallpaper & image domains

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,19 @@ const pwaConfig = withPWA({
   },
 });
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
+};
 
 export default pwaConfig(nextConfig);
